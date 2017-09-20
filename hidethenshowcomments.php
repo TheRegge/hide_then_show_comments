d27bdcad19acb2dd6dd2097c68e059821dcfd44e
Regis Zaleman <regis.zaleman@gmail.com>
2017-09-20 14:48:36 -0400

    Remove links to comments in admin toolbar and admin menu. Apply restrictions to users without 'moderate_comments' capacity (Authors and below)

--- hidethenshowcomments.php
+++ hidethenshowcomments.php
@@ -12,8 +12,20 @@ Version: 0.1
 Author URI: https://blogs.dalton.org/technofrench
 */
 
-add_action( 'the_post', 'check_and_hide_comments');
 
+// Remove 'comments' link from admin bar:
+function remove_admin_bar_menus($wp_admin_bar){
+	$wp_admin_bar->remove_node('comments');
+}
+
+// Remove comments menu page
+function remove_menus() {
+    global $menu;
+    remove_menu_page( 'edit-comments.php' );
+}
+
+// Hide comments before user has posted at
+// least one comment for the post
 function check_and_hide_comments() {
     global $post;
 
@@ -32,3 +44,12 @@ function check_and_hide_comments() {
 function change_comments_template() {
     return plugin_dir_path( __FILE__ ) . 'template-comments.php';
 }
+
+function apply_restrictions() {
+    if (! current_user_can('moderate_comments')) {
+        add_action('admin_bar_menu', 'remove_admin_bar_menus', 999);
+        add_action( 'admin_init', 'remove_menus', 999 );
+        add_action( 'the_post', 'check_and_hide_comments');
+    }
+}
+add_action('init', apply_restrictions);
